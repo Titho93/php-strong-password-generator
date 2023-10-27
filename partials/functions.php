3a65a56e9b4862fbdd7a6a0571c00a9c332397de
titho93 <titho93.10@gmail.com>
2023-10-27 16:29:13 +0200

    [add]maiusc letter

--- partials/functions.php
+++ partials/functions.php
@@ -2,7 +2,7 @@
 
 function generateCharactersRandom($number) {
     $randomPsw = '';
-    $characters = 'abcdefghijklmnopqrstuvwxyz1234567890!_?+-$%';
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!_?+-$%';
 
     for($i = 0; $i < $number; $i++){
         $randomPsw .= $characters[rand(0, strlen($characters) -1 )];
